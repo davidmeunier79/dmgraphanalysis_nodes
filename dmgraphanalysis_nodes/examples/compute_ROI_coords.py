b1c528ddf5b6784799c80f2334a908434d8daf79
david <david@david-HP-Z820-Workstation.(none)>
2014-11-17 18:12:43 +0100

    modif pour image ALS sfn

--- dmgraphanalysis_nodes/examples/compute_ROI_coords.py
+++ dmgraphanalysis_nodes/examples/compute_ROI_coords.py
@@ -27,7 +27,7 @@ import pyqtgraph as pg
 import numpy as np
 
     
-def pyplotbrain_display_ROI_coords(ROI_coords_MNI_coords_file,ROI_coords_orig_constrast_file):
+def pyplotbrain_display_ROI_coords(ROI_coords_MNI_coords_file,ROI_coords_orig_constrast_file,export_path,pref):
 
     ROI_coords_MNI_coords = np.array(np.loadtxt(ROI_coords_MNI_coords_file),dtype = 'int')
     
@@ -65,17 +65,28 @@ def pyplotbrain_display_ROI_coords(ROI_coords_MNI_coords_file,ROI_coords_orig_co
                     #(0,1,1,.8),
                     #]
 
+    #colors = [ 
+                    #(1,0,0,.8),
+                    #(1,0,0,.8),
+                    #(0,1,0,.8),
+                    #(0,1,0,.8),
+                    #]
+                    
     colors = [ 
                     (1,0,0,.8),
-                    (1,0,0,.8),
                     (0,1,0,.8),
-                    (0,1,0,.8),
+                    #(0,0,1,.8),
+                    #(0,0,1,.8),
+                    #(0,1,1,.8),
+                    #(0,1,1,.8),
                     ]
-
+                    
     #for i,code in enumerate(codes):
     #for i,code in enumerate(['E1','E2']):
-    for i,code in enumerate(['R2E1','E1','R2E2','E2']):
+    #for i,code in enumerate(['R2E1','E1','R2E2','E2']):
     #for i,code in enumerate(['R1','R2','R2E2','R2E1','E1','E2']):
+        
+    for i,code in enumerate(['R1','R2']):
         
         print i,code,colors[i]
         
@@ -85,24 +96,28 @@ def pyplotbrain_display_ROI_coords(ROI_coords_MNI_coords_file,ROI_coords_orig_co
         
         view.add_node(selected_coords, color = colors[i], size = 4)
 
-        #n = 30
         
-        #node_coords = np.random.randn(n, 3)*20
-        #connection_with = np.zeros((n,n))
-        #connection_with[1,2] = 3
-        #connection_with[4,7] = 5
-        #connection_with[5,2] = 6
-        #connection_with[8,5] = 4.5
-        #connection_with[2,4] = 6
-        #connection_with[8,2] = 6
-        
-        #view.add_edge(node_coords,connection_with,color = color)
+    view.resize(1200,800)
+    
+    ## from right
+    view.glview.setCameraPosition(distance = 250, azimuth = 0,elevation = 0)
+    view.to_file(os.path.join(export_path,pref + '_from_right.png'))
+    
+    ### from front
+    view.glview.setCameraPosition(distance = 280,azimuth = 90,elevation = 0)
+    view.to_file(os.path.join(export_path,pref + '_from_front.png'))
 
-    #view.to_file('test1.png')
-    #view.to_file('test2.jpg')
+    ### from top
+    view.glview.setCameraPosition(distance = 250,azimuth = 180,elevation = 90)
+    view.to_file(os.path.join(export_path,pref + '_from_top.png'))
+
+    ### from left
+    view.glview.setCameraPosition(distance = 250,azimuth = 180,elevation = 0)
+    view.to_file(os.path.join(export_path,pref + '_from_left.png'))
 
     app.exec_()
 
+    
 def pyplotbrain_display_one_ROI_coords(ROI_coords_MNI_coords_file):
 
     ROI_coords_MNI_coords = np.array(np.loadtxt(ROI_coords_MNI_coords_file),dtype = 'int')
@@ -137,12 +152,12 @@ if __name__ =='__main__':
         #resliced_full_HO_img_file = compute_labelled_mask_from_HO(ref_img)
             
             
-    if not os.path.exists(ROI_coords_labelled_mask_file):
+    #if not os.path.exists(ROI_coords_labelled_mask_file):
         
         ### compute mask from MNI coords 
         #compute_labelled_mask_from_ROI_coords_files(resliced_full_HO_img_file,ROI_coords_MNI_coords_file,neighbourhood = neighbourhood)
         
-        compute_labelled_mask_from_ROI_coords_files(resliced_full_HO_img_file,ROI_coords_MNI_coords_file,neighbourhood = neighbourhood)
+        #compute_labelled_mask_from_ROI_coords_files(resliced_full_HO_img_file,ROI_coords_MNI_coords_file,neighbourhood = neighbourhood)
         
         #compute_labelled_mask_from_HO_and_merged_spm_mask()
         ### compute ROI mask HO()
@@ -153,7 +168,7 @@ if __name__ =='__main__':
     ### merge coord and label files 
     #all_ROI_coords_file,all_ROI_labels_file = merge_coord_and_label_files(ROI_coords_dir)
     
-    #pyplotbrain_display_ROI_coords(ROI_coords_MNI_coords_file,ROI_coords_orig_constrast_file)
+    pyplotbrain_display_ROI_coords(ROI_coords_MNI_coords_file,ROI_coords_orig_constrast_file,export_path = ROI_coords_dir, pref = 'NodeCoords_R1R2')
     
     #pyplotbrain_display_one_ROI_coords(os.path.join(nipype_analyses_path,"ReseauALS-conj_corPos-Rec",'Coord_NetworkRec.txt'))
     
